3cf799de3116f549d9fb99945291dd3315b8819e
Swardiantara Silalahi <swardyantara@gmail.com>
2021-04-01 22:16:34 +0700

    Initial commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+// use Database\Seeders\UserTypeSeeder;
+// use Database\Seeders\UserSeeder;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call([UserTypeSeeder::class]);
+        $this->call([UserSeeder::class]);
+    }
+}

eb82863dd18cc1b3d95595f58ad73f5aec3b74e1
Swardiantara Silalahi <swardyantara@gmail.com>
2021-04-03 20:22:23 +0700

    404 handler return api-spec

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -33,9 +34,99 @@ class Handler extends ExceptionHandler
      * @return void
      */
     public function register()
-    {
-        $this->reportable(function (Throwable $e) {
-            //
+    {   
+        // $this->reportable(function (Throwable $e) {
+        //     //
+        // });
+        $this->renderable(function (NotFoundHttpException $e, $request) {
+            return response()->json([
+                "code" => 404,
+                "status" => "fail",
+                "message" => "Requested URL is not found. Try below URLs",
+                "data" => [
+                    "auth" => [
+                        "login" => [
+                            "method" => "post",
+                            "auth" => "no",
+                            "url" => "/api/auth/login",
+                            "payload" => [
+                                "email" => "string",
+                                "password" => "string"
+                            ]
+                        ],
+                        "logout" => [
+                            "method" => "post",
+                            "auth" => "yes",
+                            "authType" => "bearer-token",
+                            "url" => "/api/auth/logout"
+                        ]
+                    ],
+                    "messages" => [
+                        "sendMessage" => [
+                            "method" => "post",
+                            "url" => "/api/messages",
+                            "auth" => "yes",
+                            "authType" => "bearer-token",
+                            "payload" => [
+                                "message" => "string",
+                                "receiverId" => "int"
+                            ]
+                        ],
+                        "getAllMessagees" => [
+                            "method" => "get",
+                            "url" => "/api/messages",
+                            "auth" => "yes",
+                            "authType" => "bearer-token",
+                        ]
+                    ],
+                    "reports" => [
+                        "createReport" => [
+                            "method" => "post",
+                            "url" => "/api/reports",
+                            "auth" => "yes",
+                            "authType" => "bearer-token",
+                            "payload" => [
+                                "type" => "enum:feedback|bug",
+                                "description" => "string"
+                            ]
+                        ],
+                        "getAllReports" => [
+                            "method" => "get",
+                            "url" => "/api/reports",
+                            "auth" => "yes",
+                            "authType" => "bearer-token",
+                        ]
+                    ],
+                    "customers" => [
+                        "getAllCustomers" => [
+                            "method" => "get",
+                            "url" => "/api/customers",
+                            "auth" => "yes",
+                            "authType" => "bearer-token",
+                        ],
+                        "deleteCustomer" => [
+                            "method" => "delete",
+                            "url" => "/api/customers/{userId}",
+                            "auth" => "yes",
+                            "authType" => "bearer-token",
+                        ],
+                        "getConversationWith" => [
+                            "method" => "get",
+                            "url" => "/api/customers/conversation/{receiverId}",
+                            "auth" => "yes",
+                            "authType" => "bearer-token",
+                        ],
+                    ],
+                    "staff" => [
+                        "getConversationWith" => [
+                            "method" => "get",
+                            "url" => "/api/staff/conversation/{receiverId}",
+                            "auth" => "yes",
+                            "authType" => "bearer-token",
+                        ],
+                    ],
+                ]
+            ], 404, ['Content-Type' => 'application/json; charset=UTF-8']);
         });
     }
 }
